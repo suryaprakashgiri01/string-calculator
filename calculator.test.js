9ebb2ca2993d36057176b329b1d5fc337ebe4f23
SURYA-PRAKASH-GIRI <suryaprakashgiri0@gmail.com>
2024-10-14 11:10:17 +0530

    feat: main: handle multiple numbers

--- calculator.test.js
+++ calculator.test.js
@@ -6,12 +6,16 @@ describe("calculator", () => {
       expect(add("")).toBe(0);
     });
 
-    test('should return the number when only one number is provided', () => {
+    test("should return the number when only one number is provided", () => {
       expect(add("1")).toBe(1);
     });
 
-    test('should return the sum of two numbers separated by a comma', () => {
+    test("should return the sum of two numbers separated by a comma", () => {
       expect(add("1,2")).toBe(3);
-    });    
+    });
+
+    test("should return the sum of multiple numbers", () => {
+      expect(add("1,2,3,4")).toBe(10);
+    });
   });
 });
