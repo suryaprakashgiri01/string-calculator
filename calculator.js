7f0861f145109477bcc7614df5a516723f44a12f
SURYA-PRAKASH-GIRI <suryaprakashgiri0@gmail.com>
2024-10-14 11:25:47 +0530

    feat: main: support different delimiters

--- calculator.js
+++ calculator.js
@@ -1,6 +1,14 @@
 function add(numbers) {
   if (!numbers) return 0;
-  const numArray = numbers.replace(/\n/g, ',').split(',').map(Number);
+
+  let delimiter = ',';
+  if (numbers.startsWith('//')) {
+    const parts = numbers.split('\n');
+    delimiter = parts[0].slice(2);
+    numbers = parts[1];
+  }
+
+  const numArray = numbers.replace(/\n/g, delimiter).split(delimiter).map(Number);
   return numArray.reduce((sum, num) => sum + num, 0);
 }
 
