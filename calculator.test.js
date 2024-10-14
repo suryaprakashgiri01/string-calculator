2aea86a856218dbed1f3c7b909246aaa790d0f39
SURYA-PRAKASH-GIRI <suryaprakashgiri0@gmail.com>
2024-10-14 11:00:14 +0530

    feat: main: handle one number input

--- calculator.test.js
+++ calculator.test.js
@@ -5,5 +5,9 @@ describe("calculator", () => {
     test("should return 0 for an empty string", () => {
       expect(add("")).toBe(0);
     });
+
+    test('should return the number when only one number is provided', () => {
+      expect(add("1")).toBe(1);
+    });
   });
 });
